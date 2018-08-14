e5c1575ec175e3d33ee88290005bb5b8fcfd2219
Avante Vandaleur <33044135+AvanteVandaleur@users.noreply.github.com>
2018-08-14 01:53:24 -0400

    Fix allocate array const size 0 on MSVSC

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -121,7 +121,7 @@ static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
 /* Block heights we are going to have hard forks at */
-const uint64_t FORK_HEIGHTS[] = {};
+const uint64_t FORK_HEIGHTS[] = { 90000000 };
 
 const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
 
